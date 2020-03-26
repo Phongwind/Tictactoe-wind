a085393886c61f079286fbd12df31478025c6e78
Phongwind <phongdesign0201@gmail.com>
2020-03-26 10:03:14 +0700

    add history and winner

--- src/App.js
+++ src/App.js
@@ -2,27 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      squares: ['','','','','','','','',''],
-      nextPlayer: false
+      squares: ['', '', '', '', '', '', '', '', ''],
+      nextPlayer: false,
+      history: []
     }
   }
   setParentState = (obj) => {
     this.setState(obj)
   }
-  render () {
+
+  showPast = (item, idx) => {
+    this.setState({ squares: item.squares, nextPlayer: item.nextPlayer, history: this.state.history.filter((e, i) => i <= idx) })
+  }
+
+
+  render() {
     return (
       <div className="App">
         <h1>Tic tac toe</h1>
-        <Board {...this.state} setParentState={this.setParentState}/>
+        <div className="containter" style={{display:'flex', flexDirection: 'column'}}>
+          <div className="row">
+           
+
+            <div className="col-6">
+              <Board {...this.state} setParentState={this.setParentState} />
+            </div>
+
+            <div className="col-6" style={{marginTop: '5%'}}>
+              <ul style={{listStyleType: 'none'}}>
+                {
+                  this.state.history.map((item, idx) => {
+                    return (<li style={{marginBottom: '5%'}}><button onClick={() => this.showPast(item, idx)}>go to move {idx + 1}</button></li>)
+                  })
+                }
+              </ul>
+            </div>
+
+          </div>
+        </div>
+
+
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,45 +1,77 @@
 import React, { Component } from 'react'
 import Square from './Square'
 
+
 export default class Board extends Component {
 
-    onSquareClicked= (i) => {
+    onSquareClicked = (i) => {
         console.log("the box number you click", i)
         //1.make the one array and copy the value from parent array
         let squareList = this.props.squares.slice();
         //2.change the value at copied array
-        squareList[i]= this.props.nextPlayer?"o":"x"
+        squareList[i] = this.props.nextPlayer ? "o" : "x"
         //3 insert that array into parent array
-        this.props.setParentState({squares:squareList, nextPlayer:!this.props.nextPlayer})
+        this.props.setParentState({ squares: squareList, nextPlayer: !this.props.nextPlayer, history: [...this.props.history, { squares: squareList, nextPlayer: !this.props.nextPlayer }] })
     }
 
-
+    winnerCalculate = () => {
+        const winCase = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        for (let i = 0; i < winCase.length; i++) {
+            let [a, b, c] = winCase[i];
+            if (this.props.squares[a] && this.props.squares[a] === this.props.squares[b] && this.props.squares[a] === this.props.squares[c]) {
+                return this.props.squares[a];
+            }
+        }
+    }
 
     render() {
 
-        let status='';
-        status = this.props.nextPlayer?`nextPlayer is O`:`nextPlayer is X`;
-
+        let status = '';
+        let winner = this.winnerCalculate();
+        if (winner) {
+            status = `Winner is ${winner}`
+        } else {
+            status = this.props.nextPlayer ? `nextPlayer is O` : `nextPlayer is X`;
+        }
         return (
             <div>
-                <h2>{status}</h2>
-                <div style={{ display: 'flex' }}>
-                    <Square value={this.props.squares[0]} onClick={()=>this.onSquareClicked(0)}/>
-                    <Square value={this.props.squares[1]} onClick={()=>this.onSquareClicked(1)}/>
-                    <Square value={this.props.squares[2]} onClick={()=>this.onSquareClicked(2)}/>
-                </div>
+                
+                    
+                            <h2>{status}</h2>
+                        
 
-                <div style={{ display: 'flex' }}>
-                    <Square value={this.props.squares[3]} onClick={()=>this.onSquareClicked(3)}/>
-                    <Square value={this.props.squares[4]} onClick={()=>this.onSquareClicked(4)}/>
-                    <Square value={this.props.squares[5]} onClick={()=>this.onSquareClicked(5)}/>
-                </div>
+                       
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <Square value={this.props.squares[0]} onClick={() => this.onSquareClicked(0)} />
+                                <Square value={this.props.squares[1]} onClick={() => this.onSquareClicked(1)} />
+                                <Square value={this.props.squares[2]} onClick={() => this.onSquareClicked(2)} />
+                            </div>
 
-                <div style={{ display: 'flex' }}>
-                    <Square value={this.props.squares[6]} onClick={()=>this.onSquareClicked(6)}/>
-                    <Square value={this.props.squares[7]} onClick={()=>this.onSquareClicked(7)}/>
-                    <Square value={this.props.squares[8]} onClick={()=>this.onSquareClicked(8)}/>
-                </div>
+                            <div style={{ display: 'flex' }}>
+                                <Square value={this.props.squares[3]} onClick={() => this.onSquareClicked(3)} />
+                                <Square value={this.props.squares[4]} onClick={() => this.onSquareClicked(4)} />
+                                <Square value={this.props.squares[5]} onClick={() => this.onSquareClicked(5)} />
+                            </div>
+
+                            <div style={{ display: 'flex' }}>
+                                <Square value={this.props.squares[6]} onClick={() => this.onSquareClicked(6)} />
+                                <Square value={this.props.squares[7]} onClick={() => this.onSquareClicked(7)} />
+                                <Square value={this.props.squares[8]} onClick={() => this.onSquareClicked(8)} />
+                            </div>
+                       
+
+                   
+                
+
 
             </div>
         )
